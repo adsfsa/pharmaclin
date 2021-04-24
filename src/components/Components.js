cd23b4b86ec218e9a57dc69387ff16b0454b6dbe
Gileade Aquino Teixeira <gileadeateixeira@gmail.com>
2021-04-24 08:48:02 -0300

    Salvando

--- src/components/Components.js
+++ src/components/Components.js
@@ -38,6 +38,48 @@ export const TopoPadrao = ({setaVoltar, size}) =>{
         </TopContainer>
     );
 }
+export const TopHome = ({nomeIconeEsquerdo, setaVoltar, nomeArea}) =>{
+    return(
+        <View style={{width:'100%'}}>
+            <TopContainer>
+                <Icon
+                    name='arrow-back'
+                    color='#FFFFFF'
+                    onPress = {setaVoltar}
+                    size = {24}
+                />
+                <View >
+                    <PharmaClinLogo100x100 />
+                </View>
+                <Icon
+                    name='arrow-back'
+                    color='#00BACD'
+                    size = {24}
+                />
+            </TopContainer>
+            <View style={{width:'100%', paddingHorizontal: 40, marginTop: 15}}>
+                <ViewArea>
+                    <View style = {{paddingHorizontal: 20, flexDirection: 'row', alignItems: 'center'}}>
+                        <Icon name = {nomeIconeEsquerdo} size = {24} color= "#FE7F57"/>
+                        <Text style={{fontFamily: 'Century-Gothic', color: '#000000', fontSize: 15,paddingHorizontal: 10}}>
+                            {nomeArea}
+                        </Text>
+                     </View>
+                </ViewArea>
+            </View>
+        </View>
+    );
+}
+export const ViewArea = styled.View`
+    height: 40px;    
+    background-color: #FFFFFF;
+    align-items: center;
+    border-radius: 20px;
+    flexDirection: row;
+    justify-content: space-between;
+    margin-vertical: 10px;
+`;
+
 export const TopoInterno = ({IconeCentral, setaVoltar}) =>{
     return(
         <TopContainer>
--- src/screens/NovaConsulta/index.js
+++ src/screens/NovaConsulta/index.js
@@ -1,11 +1,281 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useState, useContext, useEffect, useRef } from 'react';
+import { UserContext } from '../../contexts/UserContext';
+import { FlatList, Alert, Text, TouchableWithoutFeedback, View, StyleSheet, TouchableOpacity, TextInput, KeyboardAvoidingView, Platform, Keyboard, TextComponent } from 'react-native';
+import styled from 'styled-components/native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Container, TopoInterno,TextStyles, TopHome } from '../../components/Components';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+const styles = StyleSheet.create({
+    item: {
+        flex: 1,
+    },
+  });
 
 export default () => {
+    const inputConsulta = useRef(null);
+
+    const [consultas, setConsultas] = useState([]);
+    const [novaConsulta, setNovaConsulta] = useState("");
+    const [compraEditavel, setConsultaEditavel] = useState(false);
+    const [selecionado, setSelecionado] = useState(null);
+
+    const Consulta = ({item, backgroundColor, editarConsulta, removerConsulta}) => {
+        const [novaAtualizacao, setNovaAtualizacao] = useState("");
+        return (
+            <Consulta style={{marginBottom: 15}}>
+                {selecionado === item
+                    ?   <TextInput
+                            style = {[styles.item, backgroundColor]}
+                            textStyle = {TextStyles.baseText}
+                            placeholder = {item}
+                            placeholderTextColor = '#000000'
+                            value = {novaAtualizacao}
+                            autoFocus = {selecionado === item? true : false}
+                            autoCapitalize = 'none'
+                            maxLength = {60}
+                            selectTextOnFocus = {true}
+                            onChangeText = {
+                                novaAtualizacao => setNovaAtualizacao(novaAtualizacao)
+                            }
+                        />
+                    :   <Text style={TextStyles.baseText}>{novaAtualizacao!=='' ? novaAtualizacao : item}</Text>
+    
+                }                                    
+                <View style={{alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row'}}>
+                    {selecionado === item
+                        ?   (
+                            novaAtualizacao!==""
+                                ?   <TouchableOpacity
+                                        style = {{marginHorizontal: 5}}
+                                        onPress = {()=> editarConsulta(item, novaAtualizacao)}
+                                    >
+                                        <Icon 
+                                            name = 'done'
+                                            size={24} color = "#000000"
+                                        />
+                                    </TouchableOpacity>
+                                :   <TouchableOpacity
+                                        style = {{marginHorizontal: 5}}
+                                        onPress = {()=> {setSelecionado(null)}}
+                                    >
+                                        <Icon 
+                                            name = 'clear'
+                                            size={24} color = "#000000"
+                                        />
+                                    </TouchableOpacity>
+                        )
+                        :   <TouchableOpacity
+                                style = {{marginHorizontal: 5}}
+                                onPress = {()=> {setSelecionado(item)}}
+                            >
+                                <Icon 
+                                    name = 'edit'
+                                    size={24} color = "#000000"
+                                />
+                            </TouchableOpacity>
+                    }                                        
+                    <TouchableOpacity
+                        style = {{marginHorizontal: 5}}
+                        onPress ={()=>removerConsulta(item)}
+                    >
+                        <Icon name='delete-forever' size={24} color = "#000000" />
+                    </TouchableOpacity>
+                </View>
+            </Consulta>
+        );
+    }
+    
+
+    async function addConsulta(){
+        if (novaConsulta === "") {
+            Alert.alert("AVISO!", "Campo vazio");
+            return;
+            
+        }
+        const buscar = consultas.filter(
+            consultas => consultas===novaConsulta);
+        if (buscar.length !== 0) {
+            Alert.alert("AVISO!", "Esta informação já existe");
+            return;
+            
+        }
+
+        setConsultas([...consultas, novaConsulta]);
+        setNovaConsulta("");
+        Keyboard.dismiss();
+    }
+
+    async function editarConsulta(itemSelecionado, novaAtualizacao) {
+        if (novaAtualizacao === "") {
+            Alert.alert("AVISO!", "Nenhuma alteração");
+            return;
+            
+        }
+        const buscar = consultas.filter(
+            consultas => consultas===novaAtualizacao);
+        if (buscar.length !== 0) {
+            Alert.alert("AVISO!", "Esta informação já existe");
+            return;
+            
+        }
+        const novos = consultas.map(item => {
+            if (item === itemSelecionado){
+                item = novaAtualizacao;
+                return item;
+            }
+            return item;
+        })
+        setConsultas(novos);
+        setSelecionado(null);
+        Keyboard.dismiss();
+    }
+
+    async function removerConsulta(item) {
+        Alert.alert(
+            "Confirmar!",
+            "Deseja remover esta informação?",
+            [
+                {
+                    text: "Cancelar",
+                    onPress: ()=> {
+                        return;
+                    },
+                    style: 'cancel'
+                },
+                {
+                    text: "Confirmar",
+                    onPress: ()=>{
+                        setLembretes(
+                            consultas.filter(consultas => consultas!==item)
+                        );
+                    }
+                }
+            ],
+            {cancelable: false}
+        );        
+    }
+
+    useEffect(()=> {
+        async function carregarConsultas(){
+           const consultas = await AsyncStorage.getItem("consultas");
+           if (consultas) {
+               setConsultas(JSON.parse(consultas));
+           }
+        }
+        carregarConsultas();
+    } ,[]);
+
+    useEffect(()=> {
+        async function salvarConsultas(){
+           AsyncStorage.setItem("consultas", JSON.stringify(consultas));
+        }
+        salvarConsultas();
+
+    } ,[consultas]);
+    
+    
+    const navigation = useNavigation();
+
+    const  Voltar = () => {
+        navigation.reset({
+            routes: [{name: 'Home'}]
+        });
+    }
+    const renderItem = ({item}) => {
+        const backgroundColor = selecionado === item? '#FFFFFF':'transparent';
+        return(
+            <TouchableWithoutFeedback onPress={()=>{}}>
+                <Consulta
+                    item = {item}
+                    editarConsulta = {editarConsulta}
+                    removerConsulta = {removerConsulta}
+                    backgroundColor = {{backgroundColor}}
+                />
+            </TouchableWithoutFeedback>
+        )                                
+    }
+
     return (
         <Container>
-            <Text>NovaConsulta</Text>
+            <View style={{flex: 1, width: '100%', marginTop: 60, alignItems: 'center'}} >
+                <TopHome nomeIconeEsquerdo={'biotech'} setaVoltar={Voltar} nomeArea={'NOVA CONSULTA'}/>
+                <FlatList
+                    style={{flex: 1, width: '100%', paddingHorizontal: 40, marginTop: 15}}
+                    data={consultas}
+                    extraData={selecionado}
+                    keyExtractor={item => item.toString()}
+                    showsVerticalScrollIndicator = {true}
+                    renderItem={renderItem}
+                />
+                <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} accessible={false}>
+                    <View style = {{width: '100%', paddingHorizontal: 40, paddingBottom: 40, paddingTop: 15}}>
+                            <AdicionarConsulta >
+                                <TextInput
+                                    style = {{flex: 1}}
+                                    ref = {inputConsulta}
+                                    textStyle = {{fontFamily: 'Century-Gothic', color: '#000000'}}
+                                    placeholder = {compraEditavel? "": "Adicione uma consulta"}
+                                    placeholderTextColor = '#000000'
+                                    value = {novaConsulta}
+                                    maxLength = {60}
+                                    selectTextOnFocus = {true}
+                                    onChangeText = {Consulta => setNovaConsulta(Consulta)}
+                                    onFocus = {()=> {setConsultaEditavel(true)}}
+                                    onBlur = {()=> {setConsultaEditavel(false)}}
+                                />
+                                {compraEditavel &&
+                                    <TouchableOpacity
+                                        style = {{marginHorizontal: 5}}
+                                        onPress={()=>addConsulta()}
+                                    >
+                                        <Icon name='add-circle-outline' size={24} color = "#000000" />
+                                    </TouchableOpacity>                            
+                                }
+                            </AdicionarConsulta>
+                    </View>
+                </TouchableWithoutFeedback>
+            </View>
         </Container>
     );
-}
+};
+
+
+
+
+
+export const Scroller = styled.ScrollView`
+    flex: 1;
+    padding: 20px;
+    width: 100%;
+`;
+export const Consulta = styled.View`
+    height: 50px;
+    background-color: #EEEEEE;
+    border-radius: 10px;
+    justify-content: space-between;
+    align-self: stretch
+    align-items: center;
+    flex-direction: row;
+    padding-horizontal: 20px;
+`;
+export const AdicionarConsulta = styled.View`
+    height: 40px;
+    border: #000000;
+    border-radius: 20px;
+    justify-content: space-between;
+    flex-direction: row;
+    align-self: stretch;
+    align-items: center;
+    padding-horizontal: 20px;
+`;
+
+export const BtnLista = styled.TouchableOpacity`
+    height: 40px;
+    border: #000000;
+    border-radius: 20px;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;     
+`;
